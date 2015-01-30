2fd2d624f4df0ecd04113111652c9b36a46dd39c
Aaron Bradley <aaronb@Aarons-MacBook-Air.local>
2015-01-30 17:42:20 -0600

    added another line

--- TestGH/ViewController.m
+++ TestGH/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"HEllo from Aaron");
+    
 }
 //////  Hello from Matt
 - (void)didReceiveMemoryWarning {
